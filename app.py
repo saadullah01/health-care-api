17d6569c1806a317ca142a34629aed7512275bfd
saadullah01 <saadu@bu.edu>
2022-02-12 21:53:53 -0500

    main app file created

---
+++ app.py
@@ -0,0 +1,5 @@
+'''
+ This file will define and execute the main flow of our
+ app. It will include all modules that will be created separately
+ and will use their functionalities accordingly.
+'''
