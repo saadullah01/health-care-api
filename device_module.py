75227dedd6011d71dbb8ec54a89285b656e7f550
saadullah01 <saadu@bu.edu>
2022-02-15 15:38:09 -0500

    updated response

--- device_module.py
+++ device_module.py
@@ -5,34 +5,44 @@
 
 def add_reading(inp):
     user_ID, device_type, reading, time, conn = None, None, None, None, None
+    response = {
+        'success': False,
+        'message': None
+    }
     # user_ID
     try:
         user_ID = int(inp['user_ID'])
     except:
-        return "Error! Invalid user_ID format"
+        response['message'] = "Error! Invalid user_ID format"
+        return response
     # device_ID
     try:
         device_type = int(inp['device_ID'])
     except:
-        return "Error! Invalid device_ID format"
+        response['message'] = "Error! Invalid device_ID format"
+        return response
     # value
     try:
         reading = float(inp['reading'])
     except:
-        return "Error! Invalid value format"
+        response['message'] = "Error! Invalid reading format"
+        return response
     # time
     try:
         time = str(inp['time'])
     except:
-        return "Error! Invalid time format"
+        response['message'] = "Error! Invalid time format"
+        return response
     # conn
     try:
         conn = inp['db']
     except:
-        return "Error! Invalid DB format"
+        response['message'] = "Error! Invalid DB connection format"
+        return response
 
     cursor = conn.cursor()
     cursor.execute('''INSERT INTO measurements(user_ID,device_ID,reading,time) VALUES(?,?,?,?);''', (user_ID, device_type, reading, time))
     conn.commit()
 
-    return "Successfull!"
+    response['message'] = "Successfull!"
+    return response
